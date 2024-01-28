67a1378fc30ab79fc0b29698870a6ed94e57fe37
Aiyualive <liuaiyu1302@gmail.com>
2024-01-28 01:24:49 +0100

    feat: add feature completeness

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 // https://solana.stackexchange.com/questions/107/how-can-i-get-the-owner-wallet-of-an-nft-mint-using-web3-js
-import {Connection, ParsedAccountData, PublicKey} from "@solana/web3.js";
+import {Connection, ParsedAccountData, PublicKey, TransactionInstruction} from "@solana/web3.js";
+import {createAssociatedTokenAccountInstruction} from "@solana/spl-token";
 
 export async function getMintOwner(connection: Connection, mint: PublicKey): Promise<PublicKey> {
     const largestAccounts = await connection.getTokenLargestAccounts(mint);
@@ -14,4 +15,20 @@ export async function getMintOwner(connection: Connection, mint: PublicKey): Pro
     const owner = (largestAccountInfo.value.data as ParsedAccountData).parsed.info.owner;
 
     return new PublicKey(owner);
+}
+
+export async function tryCreateATAIx(
+    connection: Connection,
+    payer: PublicKey,
+    ata: PublicKey,
+    owner: PublicKey,
+    mint: PublicKey,
+    tokenProgramId: PublicKey,
+): Promise<TransactionInstruction[]> {
+    const acc = await connection.getAccountInfo(ata);
+    if (acc === null) {
+        return [createAssociatedTokenAccountInstruction(payer, ata, owner, mint, tokenProgramId)];
+    } else {
+        return [];
+    }
 }
