5aac751e3d9bdcd31413663cf9c47b09934ed6c9
Aiyualive <liuaiyu1302@gmail.com>
2024-02-19 11:24:31 +0100

    feat: get basic tests to work again

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,65 +1,75 @@
-import {Keypair} from "@solana/web3.js";
-import {EpNFTService, getGlobalCollectionConfig, sendAndConfirmRawTransaction} from "../src";
+import {Keypair, PublicKey} from "@solana/web3.js";
+import {EpNFTService, getGlobalCollectionConfig, getMint, sendAndConfirmRawTransaction} from "../src";
 import {CONNECTION, getSetup} from "./setup";
 import {assert} from "chai";
 import {getDefaultMetadata} from "./getDefaultMetadata";
+import {trySetupBurgerProgramDelegate, trySetupGlobalCollectionConfig} from "./testUtils";
+import {BN} from "@coral-xyz/anchor";
+import {getTokenMetadata} from "@solana/spl-token";
 
-const {wallet, burgerProvider} = getSetup();
-
-const mint = Keypair.generate();
+const {wallet, burgerProvider, coreProvider} = getSetup();
 const metadata = getDefaultMetadata({});
 
 describe("Testing Burger Program", () => {
-    // TODO this is outdated
-    // it("Create whitelist mint", async() => {
-    //   const tx = await burgerProvider.createWhitelistMintTx({
-    //       expiryDate: metadata.expiryDate,
-    //       mint: mint.publicKey, //TODO
-    //       name: metadata.name,
-    //       symbol: metadata.symbol,
-    //       uri: metadata.uri,
-    //       globalCollectionConfig: getGlobalCollectionConfig(),
-    //   })
-    //   await sendAndConfirmRawTransaction(CONNECTION, tx, wallet.publicKey, wallet, [mint])
-    //
-    //   console.log("\n")
-    // })
+    trySetupGlobalCollectionConfig(coreProvider, wallet);
+    trySetupBurgerProgramDelegate(burgerProvider, wallet);
 
-    // it("Get epNFTs", async() => {
-    //     const epNFTs = await EpNFTService.getEpNFTs(CONNECTION, wallet.publicKey)
-    //     assert.ok(epNFTs)
-    //     assert.equal(epNFTs.length > 0, true)
-    //     console.log("\n")
-    // })
+    let mint: PublicKey;
+    it('Mint token', async () => {
+        console.log("\n \n");
+        const globalCollectionData = await coreProvider
+            .program
+            .account
+            .globalCollectionConfig
+            .fetch(coreProvider.getGlobalCollectionConfig());
 
-    // TODO outdated
-    // it("Check is Burger NFT", async() => {
-    //     const check = await EpNFTService.isBurgerNFT(CONNECTION, mint.publicKey)
-    //     assert.equal(true, check)
-    //
-    //     console.log("\n")
-    // })
+        mint = getMint(globalCollectionData.collectionCounter, new BN(0));
+        const tx = await burgerProvider.createWhitelistMintTx({
+            expiryDate: metadata.expiryDate,
+            name: metadata.name,
+            symbol: metadata.symbol,
+            uri: metadata.uri,
+            mint,
+        })
 
-    // TODO outdated
-    // it("Check not Burger NFT", async() => {
-    //     const check = await EpNFTService.isBurgerNFT(CONNECTION, Keypair.generate().publicKey)
-    //     assert.equal(false, check)
-    //
-    //     console.log("\n")
-    // })
+        await sendAndConfirmRawTransaction(CONNECTION, tx, wallet.publicKey, wallet, []);
+        console.log("Individual Mint Metadata", await getTokenMetadata(burgerProvider.provider.connection, mint));
+    });
 
-    // it("Token Game Vote", async() => {
-    //     const owner = wallet.publicKey;
-    //     const tx = await burgerProvider.tokenGameVoteTx({
-    //         mint: mint.publicKey,
-    //         message: "hello",
-    //         owner: owner,
-    //     })
-    //     await sendAndConfirmRawTransaction(CONNECTION, tx, wallet.publicKey, wallet, [])
-    //
-    //     console.log("\n")
-    // })
-    //
+
+    it("Get epNFTs", async() => {
+        const epNFTs = await EpNFTService.getEpNFTs(CONNECTION, wallet.publicKey)
+        assert.ok(epNFTs)
+        assert.equal(epNFTs.length > 0, true)
+        console.log("\n")
+    })
+
+    it("Check is Burger NFT", async() => {
+        const check = await EpNFTService.isBurgerNFT(CONNECTION, mint)
+        assert.equal(true, check)
+
+        console.log("\n")
+    })
+
+    it("Check not Burger NFT", async() => {
+        const check = await EpNFTService.isBurgerNFT(CONNECTION, Keypair.generate().publicKey)
+        assert.equal(false, check)
+
+        console.log("\n")
+    })
+
+    it("Token Game Vote", async() => {
+        const owner = wallet.publicKey;
+        const tx = await burgerProvider.tokenGameVoteTx({
+            mint: mint,
+            message: "hello",
+            owner: owner,
+        })
+        await sendAndConfirmRawTransaction(CONNECTION, tx, wallet.publicKey, wallet, [])
+
+        console.log("\n")
+    })
+
     // it("Token Burn", async() => {
     //     const owner = wallet.publicKey;
     //     const tx = await burgerProvider.burnTokenTx({
