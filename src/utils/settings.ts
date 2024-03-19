65a24adafd8a7a5d95f448190da9316f8fcd7332
Aiyualive <liuaiyu1302@gmail.com>
2024-03-19 17:46:26 +0100

    feat: add cluster endpoint and default to false skippreflight

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,3 +1,23 @@
-export const CONFIRM_OPTIONS = {skipPreflight: true}
+import {Cluster} from "@solana/web3.js";
+
+export const CONFIRM_OPTIONS = {skipPreflight: false}
 
 export const COMMITMENT = "confirmed";
+
+export function getClusterByEndpoint(
+    endpointUrl: string,
+): Cluster | "local" | "mainnet" {
+    if (endpointUrl.includes("mainnet")) {
+        return "mainnet";
+    } else if (endpointUrl.includes("devnet")) {
+        return "devnet";
+    } else if (endpointUrl.includes("local")) {
+        return "local";
+    } else if (endpointUrl.includes("127")) {
+        return "local";
+    } else if (endpointUrl.includes("8899")) {
+        return "local";
+    } else {
+        throw new Error("Unknown network");
+    }
+}
