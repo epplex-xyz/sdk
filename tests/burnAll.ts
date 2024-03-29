2e3acf92ec8b441ff30cc1a962b9a8639ff7b1dd
Aiyualive <liuaiyu1302@gmail.com>
2024-03-29 15:09:17 +0100

    update to true wns burn

--- tests/burnAll.ts
+++ tests/burnAll.ts
@@ -1,15 +1,8 @@
 import {setupGlobals} from "./utils/setup";
-import {
-    createBurnInstruction,
-    createCloseAccountInstruction,
-    getAccount,
-    getMint,
-    TOKEN_2022_PROGRAM_ID
-} from "@solana/spl-token";
+import {getMint, TOKEN_2022_PROGRAM_ID} from "@solana/spl-token";
 import {expect} from "chai";
 import {EpNFTService, sendAndConfirmRawTransaction} from "../src";
 import {ComputeBudgetProgram, PublicKey, Transaction} from "@solana/web3.js";
-import {getAtaAddressPubkey} from "../lib/utils/generic";
 
 describe('Burn All NFTs in PAYER_ADMIN', () => {
     const {wallet, burgerProvider, wenProvider} = setupGlobals()
@@ -33,29 +26,15 @@ describe('Burn All NFTs in PAYER_ADMIN', () => {
             const mint: PublicKey = myNFts[i].mint;
             console.log(`Burn ${i}:`, mint.toString())
 
-            const ata = getAtaAddressPubkey(mint, owner);
             try {
-                const res = await getMint(
+                await getMint(
                     connection,
                     mint,
                     undefined,
                     TOKEN_2022_PROGRAM_ID
                 )
             } catch (e) {
-                console.log("mint ount found", mint.toString())
-                continue
-            }
-
-
-            let ataData;
-            try {
-                ataData = await getAccount(
-                    connection,
-                    getAtaAddressPubkey(mint, owner),
-                    undefined,
-                    TOKEN_2022_PROGRAM_ID
-                );
-            } catch (e) {
+                console.log("Mint not found")
                 continue
             }
 
@@ -78,34 +57,9 @@ describe('Burn All NFTs in PAYER_ADMIN', () => {
                     ]), wallet.publicKey, wallet, []
                 );
             } catch (e) {
-                const ixs = []
-                if (Number(ataData.amount) === 1) {
-                    ixs.push(
-                        createBurnInstruction(
-                            ata,
-                            mint,
-                            owner,
-                            1,
-                            undefined,
-                            TOKEN_2022_PROGRAM_ID
-                        ),
-                    )
-                }
-
-                // not closing mint account cus we might not have the authority to do in case of WNS nft
-                ixs.push(
-                    createCloseAccountInstruction(
-                        ata,
-                        owner,
-                        owner,
-                        undefined,
-                        TOKEN_2022_PROGRAM_ID
-                    )
-                )
-                await sendAndConfirmRawTransaction(
-                    burgerProvider.provider.connection, new Transaction().add(...ixs), wallet.publicKey, wallet, []
-                );
+                console.log("Could not WNS burn")
             }
         }
     });
+
 });
