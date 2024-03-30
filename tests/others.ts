1b6ff2745b2c96785465e1722444cf8f47811153
Aiyualive <liuaiyu1302@gmail.com>
2024-03-30 16:42:36 +0100

    new test for tokenMetadataUpdate

--- tests/others.ts
+++ tests/others.ts
@@ -13,34 +13,30 @@ describe('Reset all tokens in PAYER_ADMIN', () => {
     const connection = burgerProvider.provider.connection;
     let myNFts: any[] = [];
 
-    it('Get all NFTs', async () => {
-        const group = "DY7oYBeGCcNqEGSEFGNKVM8GDiVs7iTwZaBGVTs2LCZB"
-        const g = await wenProvider.getGroupAccount(group);
-        console.log("g", g)
-        // DY7oYBeGCcNqEGSEFGNKVM8GDiVs7iTwZaBGVTs2LCZB
-        const symbol = "\uD83C\uDF54"
+    // it('Get all NFTs', async () => {
+    //     myNFts = await EpNFTService.getT22NFTs(
+    //         connection,
+    //         owner,
+    //     );
+    //     console.log("Number of NFTs", myNFts.length);
+    //     expect(myNFts).to.not.be.empty;
+    //
+    // });
 
-    });
+    // it('Get collection data', async () => {
+    //     const collectionMint = "DY7oYBeGCcNqEGSEFGNKVM8GDiVs7iTwZaBGVTs2LCZB"
+    //     const g = await wenProvider.getGroupAccount(collectionMint);
+    //     console.log("g", g)
+    // });
 
-    it('Get collection data', async () => {
-        const collectionMint = "DY7oYBeGCcNqEGSEFGNKVM8GDiVs7iTwZaBGVTs2LCZB"
-        const g = await wenProvider.getGroupAccount(collectionMint);
-        console.log("g", g)
-    });
-
-
-
-    it('Update token metadata', async () => {
-        const mint = "9dqw9Fq9Riin1fynJQrXr6eaCE6fx3a49FMGDsyowa66"
-        const symbol = "\uD83C\uDF54"
-        const uri = "https://shdw-drive.genesysgo.net/D5rr6uUDoMcKicdPZFFzKELvPNd7LppkrkB8c2pEHGKP/5000.json"
-        const name = "Burger #5000"
-
-
-
-    });
-
-
-
+    // it('Update token metadata', async () => {
+    //     const mint = new PublicKey("9dqw9Fq9Riin1fynJQrXr6eaCE6fx3a49FMGDsyowa66")
+    //     const symbol = "\uD83C\uDF54"
+    //     const uri = "https://shdw-drive.genesysgo.net/D5rr6uUDoMcKicdPZFFzKE2LvPNd7LppkrkB8c2pEHGKP/5000.json"
+    //     const name = "Burger #5000"
+    //
+    //     const tx = await burgerProvider.tokenUpdateTx({mint, name, symbol, uri})
+    //     const id = await sendAndConfirmRawTransaction(connection, tx, wallet.publicKey, wallet, []);
+    // });
 
 });
