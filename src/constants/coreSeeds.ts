faaf489f48f2594f61d5ac82376d8791135f7dea
Aiyualive <liuaiyu1302@gmail.com>
2024-04-28 17:38:24 +0200

    acommodate anchor30

--- src/constants/coreSeeds.ts
+++ src/constants/coreSeeds.ts
@@ -1,86 +1,109 @@
-import {BN, Provider} from "@coral-xyz/anchor";
-import {PublicKey} from "@solana/web3.js";
-import {CORE_PROGRAM_ID} from "./ids";
+import { BN, Provider } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { CORE_PROGRAM_ID } from "./ids";
 import * as anchor from "@coral-xyz/anchor";
-import {EpplexCore, IDL as CoreIdl} from "../types/epplexCoreTypes";
+import EpplexCoreIdl from "../idl/epplex_core.json";
+import { EpplexCore } from "../types/epplexCoreTypes";
 
-export type CoreProgram = anchor.Program < EpplexCore >;
-export function getEpplexCoreProgram(provider: Provider, coreProgramId: PublicKey) {
-    return new anchor.Program(CoreIdl, coreProgramId, provider);
+export type CoreProgram = anchor.Program<EpplexCore>;
+export function getEpplexCoreProgram(
+    provider: Provider,
+    coreProgramId: PublicKey,
+) {
+    const idl = {
+        ...EpplexCoreIdl,
+        address: coreProgramId.toString(),
+    } as anchor.Idl;
+    return new anchor.Program(idl, provider);
 }
-export const SEED_GLOBAL_COLLECTION = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        // GLOBAL_COLLECTION
-        return obj.name === "SEED_GLOBAL_COLLECTION_CONFIG";
+export const SEED_GLOBAL_COLLECTION = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            // GLOBAL_COLLECTION
+            return obj.name === "SEED_GLOBAL_COLLECTION_CONFIG";
+        })[0].value,
+    ),
+);
 
-    })[0].value
-));
+export const SEED_COLLECTION_CONFIG = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            //  CONFIG
+            return obj.name === "SEED_COLLECTION_CONFIG";
+        })[0].value,
+    ),
+);
 
-export const SEED_COLLECTION_CONFIG = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        //  CONFIG
-        return obj.name === "SEED_COLLECTION_CONFIG";
-    })[0].value
-));
+export const SEED_COLLECTION_MINT = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            // COLLECTION_MINT
+            return obj.name === "SEED_COLLECTION_MINT";
+        })[0].value,
+    ),
+);
 
-export const SEED_COLLECTION_MINT = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        // COLLECTION_MINT
-        return obj.name === "SEED_COLLECTION_MINT";
-    })[0].value
-));
+export const SEED_MINT = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            // MINT
+            return obj.name === "SEED_MINT";
+        })[0].value,
+    ),
+);
 
-
-export const SEED_MINT = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        // MINT
-        return obj.name === "SEED_MINT";
-    })[0].value
-));
-
-
-export function getGlobalCollectionConfig(coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getGlobalCollectionConfig(
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [globalCollectionConfig] = PublicKey.findProgramAddressSync(
         [SEED_GLOBAL_COLLECTION],
-        coreProgramId
+        coreProgramId,
     );
     return globalCollectionConfig;
 }
 
 // From globalCollection counter
-export function getCollectionConfig(collectionCounter: number, coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getCollectionConfig(
+    collectionCounter: number,
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [globalCollectionConfig] = PublicKey.findProgramAddressSync(
         [
             SEED_COLLECTION_CONFIG,
-            new BN(collectionCounter).toArrayLike(Buffer, "le", 8)
+            new BN(collectionCounter).toArrayLike(Buffer, "le", 8),
         ],
-        coreProgramId
+        coreProgramId,
     );
     return globalCollectionConfig;
 }
 
-export function getCollectionMint(collectionCounter: number, coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getCollectionMint(
+    collectionCounter: number,
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [globalCollectionConfig] = PublicKey.findProgramAddressSync(
         [
             SEED_COLLECTION_MINT,
             new BN(collectionCounter).toArrayLike(Buffer, "le", 8),
         ],
-        coreProgramId
+        coreProgramId,
     );
     return globalCollectionConfig;
 }
 
-
-
 // From globalCollection counter
-export function getMint(collectionCounter: number, mintCount: number, coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getMint(
+    collectionCounter: number,
+    mintCount: number,
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [globalCollectionConfig] = PublicKey.findProgramAddressSync(
         [
             SEED_MINT,
             new BN(collectionCounter).toArrayLike(Buffer, "le", 8),
-            new BN(mintCount).toArrayLike(Buffer, "le", 8)
+            new BN(mintCount).toArrayLike(Buffer, "le", 8),
         ],
-        coreProgramId
+        coreProgramId,
     );
     return globalCollectionConfig;
 }
@@ -89,52 +112,56 @@ export function getMint(collectionCounter: number, mintCount: number, coreProgra
  * Ephemeral Membership
  */
 
-export const SEED_EPHEMERAL_DATA = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        return obj.name === "SEED_EPHEMERAL_DATA";
-    })[0].value
-));
+export const SEED_EPHEMERAL_DATA = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            return obj.name === "SEED_EPHEMERAL_DATA";
+        })[0].value,
+    ),
+);
 
-export const SEED_EPHEMERAL_AUTH = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        return obj.name === "SEED_EPHEMERAL_AUTH";
-    })[0].value
-));
+export const SEED_EPHEMERAL_AUTH = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            return obj.name === "SEED_EPHEMERAL_AUTH";
+        })[0].value,
+    ),
+);
 
-export const SEED_EPHEMERAL_RULE = Buffer.from(JSON.parse(
-    CoreIdl.constants.filter(obj => {
-        return obj.name === "SEED_EPHEMERAL_RULE";
-    })[0].value
-));
+export const SEED_EPHEMERAL_RULE = Buffer.from(
+    JSON.parse(
+        EpplexCoreIdl.constants.filter((obj) => {
+            return obj.name === "SEED_EPHEMERAL_RULE";
+        })[0].value,
+    ),
+);
 
-export function getEphemeralData(membership: PublicKey, coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getEphemeralData(
+    membership: PublicKey,
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [data] = PublicKey.findProgramAddressSync(
-        [
-            SEED_EPHEMERAL_DATA,
-            membership.toBuffer(),
-        ],
-        coreProgramId
+        [SEED_EPHEMERAL_DATA, membership.toBuffer()],
+        coreProgramId,
     );
     return data;
 }
 
 export function getEphemeralAuth(coreProgramId = CORE_PROGRAM_ID): PublicKey {
     const [auth] = PublicKey.findProgramAddressSync(
-        [
-            SEED_EPHEMERAL_AUTH
-        ],
-        coreProgramId
+        [SEED_EPHEMERAL_AUTH],
+        coreProgramId,
     );
     return auth;
 }
 
-export function getEphemeralRule(seed: BN, coreProgramId = CORE_PROGRAM_ID): PublicKey {
+export function getEphemeralRule(
+    seed: BN,
+    coreProgramId = CORE_PROGRAM_ID,
+): PublicKey {
     const [rule] = PublicKey.findProgramAddressSync(
-        [
-            SEED_EPHEMERAL_RULE,
-            seed.toArrayLike(Buffer, "le", 8),
-        ],
-        coreProgramId
+        [SEED_EPHEMERAL_RULE, seed.toArrayLike(Buffer, "le", 8)],
+        coreProgramId,
     );
     return rule;
 }
--- src/constants/ids.ts
+++ src/constants/ids.ts
@@ -1,7 +1,7 @@
-import {PublicKey} from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import EpplexBurgerIdl from "../idl/epplex_burger.json";
 import EpplexCoreIdl from "../idl/epplex_core.json";
 
-export const BURGER_PROGRAM_ID = new PublicKey(EpplexBurgerIdl.metadata.address);
+export const BURGER_PROGRAM_ID = new PublicKey(EpplexBurgerIdl.address);
 
-export const CORE_PROGRAM_ID = new PublicKey(EpplexCoreIdl.metadata.address);
+export const CORE_PROGRAM_ID = new PublicKey(EpplexCoreIdl.address);
