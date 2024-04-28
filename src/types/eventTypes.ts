b44a32d05c39bc33869495dce5b3a7a7ba21dc49
Aiyualive <liuaiyu1302@gmail.com>
2024-04-29 01:58:18 +0200

    fix: update to any

--- src/types/eventTypes.ts
+++ src/types/eventTypes.ts
@@ -1,11 +1,20 @@
-import { IdlEvents } from "@coral-xyz/anchor";
-import { EpplexBurger } from "../types/epplexBurgerTypes";
+// import { IdlEvents } from "@coral-xyz/anchor";
+// import { EpplexBurger } from "../types/epplexBurgerTypes";
 
-export type TokenGameVoteEvent = IdlEvents<EpplexBurger>["EvTokenGameVote"];
-export type TokenGameBurnEvent = IdlEvents<EpplexBurger>["EvTokenGameBurn"];
-export type TokenGameResetEvent = IdlEvents<EpplexBurger>["EvTokenGameReset"];
-export type TokenGameImmunityEvent = IdlEvents<EpplexBurger>["EvTokenGameImmunity"];
-export type GameStartEvent = IdlEvents<EpplexBurger>["EvGameStart"];
-export type GameEndEvent = IdlEvents<EpplexBurger>["EvGameEnd"];
-export type GameUpdateEvent = IdlEvents<EpplexBurger>["EvGameUpdate"];
-export type AllEventNames = EpplexBurger["events"][number]["name"]
+// export type TokenGameVoteEvent = IdlEvents<EpplexBurger>["EvTokenGameVote"];
+// export type TokenGameBurnEvent = IdlEvents<EpplexBurger>["EvTokenGameBurn"];
+// export type TokenGameResetEvent = IdlEvents<EpplexBurger>["EvTokenGameReset"];
+// export type TokenGameImmunityEvent = IdlEvents<EpplexBurger>["EvTokenGameImmunity"];
+// export type GameStartEvent = IdlEvents<EpplexBurger>["EvGameStart"];
+// export type GameEndEvent = IdlEvents<EpplexBurger>["EvGameEnd"];
+// export type GameUpdateEvent = IdlEvents<EpplexBurger>["EvGameUpdate"];
+// export type AllEventNames = EpplexBurger["events"][number]["name"]
+
+export type TokenGameVoteEvent = any;
+export type TokenGameBurnEvent = any;
+export type TokenGameResetEvent = any;
+export type TokenGameImmunityEvent = any;
+export type GameStartEvent = any;
+export type GameEndEvent = any;
+export type GameUpdateEvent = any;
+export type AllEventNames = any;
