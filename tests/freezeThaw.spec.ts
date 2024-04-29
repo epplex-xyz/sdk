06d0787b96e29dae71ca0040387d341034d6e5cc
Aiyualive <liuaiyu1302@gmail.com>
2024-04-29 07:27:59 +0200

    add thaw all

--- tests/freezeThaw.spec.ts
+++ tests/freezeThaw.spec.ts
@@ -1,6 +1,11 @@
-import { sendAndConfirmRawTransaction } from "../src";
+import {
+    EpNFTService,
+    getTransactionSize,
+    sendAndConfirmRawTransaction,
+} from "../src";
 import { setupGlobals } from "./utils/setup";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, Transaction } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("Test freeze and thaw", () => {
     const { wallet, burgerProvider } = setupGlobals();
@@ -20,14 +25,46 @@ describe("Test freeze and thaw", () => {
     //     );
     // });
 
-    it("Thawing", async () => {
-        const tx = await burgerProvider.tokenThawTx({ mint: pubkey });
-        await sendAndConfirmRawTransaction(
-            connection,
-            tx,
-            wallet.publicKey,
-            wallet,
-            [],
-        );
-    });
+    // it("Thaw", async () => {
+    //     const tx = await burgerProvider.tokenThawTx({ mint: pubkey });
+    //     await sendAndConfirmRawTransaction(
+    //         connection,
+    //         tx,
+    //         wallet.publicKey,
+    //         wallet,
+    //         [],
+    //     );
+    // });
+
+    // it("Thaw All", async () => {
+    //     const myNFts = await EpNFTService.getT22NFTs(
+    //         connection,
+    //         wallet.publicKey,
+    //     );
+    //     console.log("Number of NFTs", myNFts.length);
+    //
+    //     for (let i = 0; i < myNFts.length; i++) {
+    //         const mint: PublicKey = myNFts[i].mint;
+    //         console.log(i, mint.toString());
+    //         // TODO paste collectionMint in here, excludes the collection mint
+    //         const isCollectionMint =
+    //             mint.toString() ===
+    //             "8kNVHWjGPT241f4oeUZQxhemJFir7pyxqXAMy32Bksj1";
+    //         if (isCollectionMint) {
+    //             continue;
+    //         }
+    //
+    //         const tx = await burgerProvider.tokenThawTx({
+    //             mint,
+    //             owner: wallet.publicKey,
+    //         });
+    //         await sendAndConfirmRawTransaction(
+    //             connection,
+    //             tx,
+    //             wallet.publicKey,
+    //             wallet,
+    //             [],
+    //         );
+    //     }
+    // });
 });
