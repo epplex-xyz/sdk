d0901d07f0af3ad0a4f5be089465fc08debcc8c0
Aiyualive <liuaiyu1302@gmail.com>
2024-06-08 20:19:56 +0300

    prettier

--- tests/setupUtils.ts
+++ tests/setupUtils.ts
@@ -1,32 +1,42 @@
 import {
     CoreProvider,
-    EpplexProvider, getWnsNftTransferIxs,
-    sendAndConfirmRawTransaction, sendAndConfirmRawVersionedTransaction
+    EpplexProvider,
+    getWnsNftTransferIxs,
+    sendAndConfirmRawTransaction,
+    sendAndConfirmRawVersionedTransaction,
 } from "../src";
 import WenProvider from "../src/WenProvider";
-import {expect} from "chai";
-import {ComputeBudgetProgram, Keypair, PublicKey, Transaction, TransactionInstruction} from "@solana/web3.js";
-import {WnsGroupMintParams} from "../src/types/EpplexProviderTypes";
-import {EpMintParams} from "../src/types/EpplexProviderTypes";
-import {PAYER_ADMIN} from "../src/constants/keys";
+import { expect } from "chai";
+import {
+    ComputeBudgetProgram,
+    Keypair,
+    PublicKey,
+    Transaction,
+    TransactionInstruction,
+} from "@solana/web3.js";
+import { WnsGroupMintParams } from "../src/types/EpplexProviderTypes";
+import { EpMintParams } from "../src/types/EpplexProviderTypes";
+import { PAYER_ADMIN } from "../src/constants/keys";
 // @ts-ignore
 import fs from "fs";
 
-export function trySetupGlobalCollectionConfig(
-    provider: CoreProvider,
-    wallet,
-) {
-    it('Try create global collection config', async () => {
+export function trySetupGlobalCollectionConfig(provider: CoreProvider, wallet) {
+    it("Try create global collection config", async () => {
         try {
-            const globalCollectionData = await provider
-                .program
-                .account
-                .globalCollectionConfig
-                .fetch(provider.getGlobalCollectionConfig());
+            const globalCollectionData =
+                await provider.program.account.globalCollectionConfig.fetch(
+                    provider.getGlobalCollectionConfig(),
+                );
             // console.log("Global collection config data", globalCollectionData)
         } catch (e) {
             const tx = await provider.createGlobalCollectionConfigTx();
-            const id = await sendAndConfirmRawTransaction(provider.provider.connection, tx, wallet.publicKey, wallet, []);
+            const id = await sendAndConfirmRawTransaction(
+                provider.provider.connection,
+                tx,
+                wallet.publicKey,
+                wallet,
+                [],
+            );
             expect(id).to.be.not.empty;
         }
     });
@@ -36,47 +46,49 @@ export function trySetupBurgerProgramDelegate(
     provider: EpplexProvider,
     wallet,
 ) {
-    it("Try create burger delegate ", async() => {
+    it("Try create burger delegate ", async () => {
         try {
-            const burgerDelegateData = await provider
-                .program
-                .account
-                .programDelegate
-                .fetch(provider.getProgramDelegate());
+            const burgerDelegateData =
+                await provider.program.account.programDelegate.fetch(
+                    provider.getProgramDelegate(),
+                );
             // console.log("Program Delegate Data", burgerDelegateData)
         } catch (e) {
             const tx = await provider.createProgramDelegateTx();
-            const id = await sendAndConfirmRawTransaction(provider.provider.connection, tx, wallet.publicKey, wallet, []);
+            const id = await sendAndConfirmRawTransaction(
+                provider.provider.connection,
+                tx,
+                wallet.publicKey,
+                wallet,
+                [],
+            );
             expect(id).to.be.not.empty;
         }
-    })
+    });
 }
 
-
-export function trySetupGameConfig(
-    provider: EpplexProvider,
-    wallet,
-) {
-    it("Try create game config ", async() => {
+export function trySetupGameConfig(provider: EpplexProvider, wallet) {
+    it("Try create game config ", async () => {
         try {
-            const gameConfig = await provider
-                .program
-                .account
-                .gameConfig
-                .fetch(provider.getGameConfig());
+            const gameConfig = await provider.program.account.gameConfig.fetch(
+                provider.getGameConfig(),
+            );
         } catch (e) {
             const tx = await provider.gameCreateTx();
-            const id = await sendAndConfirmRawTransaction(provider.provider.connection, tx, wallet.publicKey, wallet, []);
+            const id = await sendAndConfirmRawTransaction(
+                provider.provider.connection,
+                tx,
+                wallet.publicKey,
+                wallet,
+                [],
+            );
             expect(id).to.be.not.empty;
         }
-    })
+    });
 }
 
-export function trySetupManagerAccount(
-    provider: WenProvider,
-    wallet,
-) {
-    it("Try init manager account ", async() => {
+export function trySetupManagerAccount(provider: WenProvider, wallet) {
+    it("Try init manager account ", async () => {
         const acc = await provider.getManagerAccount();
         // Manager account already exists
         if (acc !== undefined) {
@@ -84,9 +96,15 @@ export function trySetupManagerAccount(
         }
 
         const tx = await provider.initManagerAccountTx();
-        const id = await sendAndConfirmRawTransaction(provider.provider.connection, tx, wallet.publicKey, wallet, []);
+        const id = await sendAndConfirmRawTransaction(
+            provider.provider.connection,
+            tx,
+            wallet.publicKey,
+            wallet,
+            [],
+        );
         expect(id).to.be.not.empty;
-    })
+    });
 }
 
 export function setupCollection(
@@ -104,16 +122,16 @@ export function setupCollection(
     const baseSeed = seed ?? Math.floor(Math.random() * 100000);
 
     it("Creates a new rule if not created already", async () => {
-        const res = await core.getRuleData(baseSeed)
+        const res = await core.getRuleData(baseSeed);
         if (res !== undefined) {
-            console.log("Rule already created")
-            return
+            console.log("Rule already created");
+            return;
         }
 
         // Compute budgest for rule create
         const ixs = [
             ComputeBudgetProgram.setComputeUnitLimit({
-                units: 200_000
+                units: 200_000,
             }),
             ComputeBudgetProgram.setComputeUnitPrice({
                 microLamports: 50_000,
@@ -122,23 +140,33 @@ export function setupCollection(
         const tx = await burger.ephemeralRuleCreateTx({
             seed: baseSeed,
             renewalPrice: 1000_000_000_000, // 1000 SOL
-            treasury: PAYER_ADMIN
-        })
+            treasury: PAYER_ADMIN,
+        });
 
         const id = await sendAndConfirmRawTransaction(
-            burger.provider.connection, new Transaction().add(...ixs, tx), wallet.publicKey, wallet, []
-        )
+            burger.provider.connection,
+            new Transaction().add(...ixs, tx),
+            wallet.publicKey,
+            wallet,
+            [],
+        );
         expect(id).to.not.be.empty;
     });
 
     it("Create a collection", async () => {
-        const tx = await burger.wnsGroupMintTx(collectionArgs)
-        const id = await sendAndConfirmRawVersionedTransaction(burger.provider.connection, tx.instructions, wallet.publicKey, wallet, [collectionMint]);
+        const tx = await burger.wnsGroupMintTx(collectionArgs);
+        const id = await sendAndConfirmRawVersionedTransaction(
+            burger.provider.connection,
+            tx.instructions,
+            wallet.publicKey,
+            wallet,
+            [collectionMint],
+        );
         expect(id).to.not.be.empty;
     });
 
     it("Max mint nfts into collection", async () => {
-        for(let i = 0; i < collectionArgs.maxSize; i++){
+        for (let i = 0; i < collectionArgs.maxSize; i++) {
             const mint = Keypair.generate();
             console.log(`mint ${i + 1}`, mint.publicKey.toString());
 
@@ -146,9 +174,10 @@ export function setupCollection(
                 ...epMintArgs,
                 groupMint: collectionMint.publicKey,
                 mint: mint.publicKey,
+                addPermanentDelegate: true,
                 computeBudget: 600_000,
                 addGameReset: true,
-            }
+            };
 
             // Mint
             const mintTx = await burger.wnsMemberMintTx(mintArgs);
@@ -162,25 +191,29 @@ export function setupCollection(
             });
 
             // Transfer
-            let transferIxs: TransactionInstruction[] = []
+            let transferIxs: TransactionInstruction[] = [];
             if (receiver !== undefined) {
                 transferIxs = getWnsNftTransferIxs({
                     mint: mint.publicKey,
                     sender: wallet.publicKey,
                     payer: wallet.publicKey,
-                    receiver
-                })
+                    receiver,
+                });
             }
 
             const id = await sendAndConfirmRawVersionedTransaction(
-                burger.provider.connection, [
+                burger.provider.connection,
+                [
                     ...mintTx.instructions,
                     ...membershipTx.instructions,
-                    ...transferIxs
-                ], wallet.publicKey, wallet, [mint]
+                    ...transferIxs,
+                ],
+                wallet.publicKey,
+                wallet,
+                [mint],
             );
             expect(id).to.not.be.empty;
-            mints.push(mint.publicKey)
+            mints.push(mint.publicKey);
         }
 
         // Create collection config file
@@ -192,11 +225,11 @@ export function setupCollection(
         fs.mkdirSync("tests/.output", { recursive: true });
         fs.writeFileSync(
             "tests/.output/collection_config.json",
-            JSON.stringify(collectionJSON, null, 2)
+            JSON.stringify(collectionJSON, null, 2),
         );
 
         expect(mints.length).to.be.equal(collectionArgs.maxSize);
     });
 
-    return mints
+    return mints;
 }
